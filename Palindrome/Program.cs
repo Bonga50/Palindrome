961f32e4348b452278c5640d8b75acb2bf2aa768
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-10-10 09:41:16 +0000

    Added binary search

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -7,7 +7,7 @@ namespace Palindrome
         static void Main(string[] args)
         {
 
-            //List<string> word = new List<string>() {"Dad","Man","Mom","Sister", "civic", "radar", "level", "rotor", "kayak", "madam", "and", "refer" };
+            List<string> word = new List<string>() {"Dad","Man","Mom","Sister", "civic", "radar", "level", "rotor", "kayak", "madam", "and", "refer" };
             //for (int i = 0; i < word.Count; i++)
             //{
             //    Console.WriteLine(checkPalindrome(word[i]));
@@ -15,12 +15,41 @@ namespace Palindrome
             //}
             //int nth = 6;
             //Fibonaci(nth,0,0,1,1);
-            string test = "ABC";
-            permutations(test.ToArray(),0,0);
-            
+            List<int> nums = new List<int>() {1,2,3,4,5,6,7,8,9,10 };
+            //string test = "ABC";
+            //permutations(test.ToArray(),0,0);
+
+            BinarySearch(8, nums, 0, 0, nums.Count - 1);
+
 
         }
+        public static void BinarySearch(int target, List<int> nums, int mid, int start, int end)
+        {
+            //bool result=false;
+            if (nums.Count == 0 || start >= nums.Count() - 1) {
+                Console.WriteLine("Not found");
+                return; }
 
+
+            //finding middle
+            mid = start + (end - start) / 2;
+
+            if (nums[mid]==target){
+                Console.WriteLine("Number found at index : "+mid );
+                return;
+            }
+        
+            if (target > nums[mid])
+            {
+                start = mid+1;
+            }else if (target < nums[mid])
+            {
+                end = mid-1;
+            }
+            BinarySearch(target, nums, mid, start, end);
+        
+
+        }
         public static void Fibonaci(int Term,int count,int current,int prev,int next) {
 
             
